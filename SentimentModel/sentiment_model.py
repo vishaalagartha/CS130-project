2d0c80551516343571d10930b44a42587d4cd5b7
aanava <anav.krish@gmail.com>
2018-11-15 11:43:56 -0800

    Update sentiment_model.py

--- SentimentModel/sentiment_model.py
+++ SentimentModel/sentiment_model.py
@@ -14,35 +14,29 @@ class SentimentModel:
         print('handling comments')
         
         #parse through comments using Subject-Predicate-Object and split comment into phrases
-        #return phrases.
+        #still under work
 
-        comments_ = ["VADER is smart, VADER is handsome, and VADER is funny.",  # positive sentence example
-             "VADER is smart, VADER is handsome, and VADER is funny!",  # punctuation emphasis handled correctly (sentiment intensity adjusted)
-             "VADER is very smart, and Vader is very handsome and Vader is very funny.", # booster words handled correctly (sentiment intensity adjusted)
-             "VADER is VERY SMART, VADER is VERY handsome, and VADER is VERY FUNNY.",  # emphasis for ALLCAPS handled
-             "VADER is VERY SMART, VADER is VERY handsome, and VADER is VERY FUNNY!!!", # combination of signals - VADER appropriately adjusts intensity
-             "VADER is VERY SMART, VADER is uber handsome, and VADER is FRIGGIN FUNNY!!!", # booster words & punctuation make this close to ceiling for score
-             "VADER is not smart, VADER is not handsome, VADER is not funny.",  # negation sentence example
-             "The book was good.",  # positive sentence
-             "At least it isn't a horrible book.",  # negated negative sentence with contraction
-             "The book was only kind of good.", # qualified positive sentence is handled correctly (intensity adjusted)
-             "The plot was good, but the characters are uncompelling and the dialog is not great.", # mixed negation sentence
-             "Today SUX!",  # negative slang with capitalization emphasis
-             "Today only kinda sux! But I'll get by, lol", # mixed sentiment example with slang and constrastive conjunction "but"
-             "Make sure you :) or :D today!",  # emoticons handled
-             "Catch utf-8 emoji such as such as 💘 and 💋 and 😁",  # emojis handled
-             "Not bad at all",  # Capitalized negation,
-             ]
-        print(comments_)
-        return comments_  
+        #return phrases.
+        phrases = []
+        for i in comments:
+            phrase = re.split('; |, |and |but |or |nor ',i)
+
+            for j in phrase:
+                phrases.append(j)
+        return phrases  
 
     def generateSentiments(self, sentences):
         print('generating sentiments')
         # Run it through the SentimentIntensityAnalyzer
-        for sentence in sentences:
-            vs = self.model_.polarity_scores(sentence)
-            print("{:-<65} {}".format(sentence, str(vs)))
-            print()
+        related_phrases = []
+        for phrase in self.comments_:
+            if word.lower() in phrase.lower():
+                related_phrases.append(phrase)
+        print(related_phrases)
+
+        for i in related_phrases:
+            vs = self.model_.polarity_scores(i)
+            print("{:-<65} {}".format(i, str(vs)))
 
     def sendSentiments(self):
         print('sending sentiments')
