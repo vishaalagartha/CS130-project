38cae9d2fb3925a9f390a0104a998823e12612bd
kzhangg <zhangkaren@g.ucla.edu>
2018-11-27 02:59:51 -0800

    more edits, connected to local .json

--- SentimentChart/chart/src/App.js
+++ SentimentChart/chart/src/App.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chart from './components/Chart';
+import data from "./components/data.json";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{},
+      word: data.words[0].name,
+      x: data.words[0].x,
+      y: data.words[0].y,
+      s: data.words[0].s,
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+
+    this.setState({
+      chartData:{
+        labels: this.state.x,
+        datasets:[
+          {
+            label: this.state.word,
+            data:this.state.y,
+            backgroundColor:this.state.s,
+          }
+        ]
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-
-          <Chart />
-
+        <Chart chartData={this.state.chartData} word={this.state.word} legendPosition="bottom"/>
       </div>
     );
   }
--- SentimentChart/chart/src/components/Chart.js
+++ SentimentChart/chart/src/components/Chart.js
@@ -1,68 +1,35 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
-// import data from './data.json';
-
-const x = ['January 2017', 'February 2017', 'March 2017', 'April 2017', 'May 2017', 'June 2017', 'July 2017', 'August 2017'];
-const y = [123, 115, 120, 153, 106, 132, 143, 152];
-
-const sent = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  //have like a switch statement and pre-determine sentiment color range
-
 
 class Chart extends Component{
-
   constructor(props){
     super(props);
     this.state = {
-      chartData: {
-
-          labels: x,
-          datasets:[{
-
-
-            label:'frequency',
-
-            data: y,
-
-            backgroundColor:[
-              'rgba(137, 15, 15, 1)',
-              'rgba(201, 16, 16, 1)',
-              'rgba(242, 140, 140, 1)',
-              'rgba(182, 244, 151, 1)',
-              'rgba(122, 188, 88, 1)',
-              'rgba(71, 145, 34, 1)',
-              'rgba(56, 132, 18, 1)',
-              'rgba(25, 73, 1, 1)',
-
-            ],
-            borderWidth:1,
-            borderColor:'#777',
-            hoverBorderWidth:3,
-            hoverBorderColor:'#000',
-
-
-          }]
-
-      }
+      chartData:props.chartData
     }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    word:'City'
   }
 
   render(){
     return (
-      //JSX but pretty much HTML
       <div className="chart">
         <Bar
           data={this.state.chartData}
           options={{
             title:{
-              display:true,
-              text: 'potato',
-              fontSize: 25
+              display:this.props.displayTitle,
+              text:'word: '+this.props.word,
+              fontSize:25
             },
             legend:{
-              display: true,
-              position: 'right'
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
             },
             scales :{
               xAxes: [{
@@ -75,7 +42,6 @@ class Chart extends Component{
       </div>
     )
   }
-
 }
 
 export default Chart;
