09dfea630d4aad8853790e3670b48a6dfe4880a4
kzhangg <zhangkaren@g.ucla.edu>
2018-11-28 15:34:19 -0800

    added mapping to sentiment values and colors

--- SentimentChart/chart/src/App.js
+++ SentimentChart/chart/src/App.js
@@ -21,6 +21,45 @@ class App extends Component {
   }
 
   getChartData(){
+    var sentiments = this.state.s;
+    var colors = [];
+    colors.length = sentiments.length;
+
+    var i;
+    for(i = 0; i < sentiments.length; i++){
+      if(sentiments[i] <= -0.8){
+        colors[i] = "rgba(92, 10, 10, 1)";
+      }
+      else if(sentiments[i] > -0.8 && sentiments[i] <= -0.6){
+        colors[i] = "rgba(137, 15, 15, 1)";
+      }
+      else if(sentiments[i] > -0.6 && sentiments[i] <= -0.4){
+          colors[i] = "rgba(201, 16, 16, 1)";
+      }
+      else if(sentiments[i] > -0.4 && sentiments[i] <= -0.2){
+        colors[i] = "rgba(242, 140, 140, 1)";
+      }
+      else if(sentiments[i] > -0.2 && sentiments[i] <= 0){
+          colors[i] = "rgba(255, 230, 230, 1)";
+      }
+      else if(sentiments[i] > 0 && sentiments[i] <= 0.2){
+        colors[i] = "rgba(159, 237, 120, 1)";
+      }
+      else if(sentiments[i] > 0.2 && sentiments[i] <= 0.4){
+          colors[i] = "rgba(95, 224, 31, 1)";
+      }
+      else if(sentiments[i] > 0.4 && sentiments[i] <= 0.6){
+        colors[i] = "rgba(56, 132, 18, 1)";
+      }
+      else if(sentiments[i] > 0.6 && sentiments[i] <= 0.8){
+          colors[i] = "rgba(25, 73, 1, 1)";
+      }
+      else if(sentiments[i] > 0.8 && sentiments[i] <= 1){
+        colors[i] = "rgba(17, 50, 1, 1)";
+      }
+
+
+    }
 
     this.setState({
       chartData:{
@@ -29,7 +68,7 @@ class App extends Component {
           {
             label: this.state.word,
             data:this.state.y,
-            backgroundColor:this.state.s,
+            backgroundColor: colors,
           }
         ]
       }
